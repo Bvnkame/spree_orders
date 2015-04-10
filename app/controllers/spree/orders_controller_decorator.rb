a1c4b977030e09966598686286bf7547ee2907de
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-04-10 18:33:20 +0700

    update

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -46,9 +46,8 @@ Spree::Api::OrdersController.class_eval do
 
 	def mine_past
 		if current_api_user.persisted?
-			@past_line_items = Spree::LineItem.select("spree_line_items.id, spree_line_items.quantity, spree_line_items.price, spree_line_items.delivery_date, spree_line_items.product_id,spree_line_items.box_id, spree_line_items.order_id, spree_orders.state, spree_orders.user_id")
-			.joins(:order).where(:spree_orders => {:state => "complete", :user_id => current_api_user.id})
-			
+			@past_orders = Spree::Order.where(:state => "complete")
+
 			render "spree/api/orders/mine_past"
 		else
 			render "spree/api/errors/unauthorized", status: :unauthorized
@@ -57,9 +56,7 @@ Spree::Api::OrdersController.class_eval do
 
 	def mine_upcoming
 		if current_api_user.persisted?
-			@upcoming_line_items = Spree::LineItem.select("spree_line_items.id, spree_line_items.quantity, spree_line_items.price, spree_line_items.delivery_date, spree_line_items.product_id,spree_line_items.box_id, spree_line_items.order_id, spree_orders.state, spree_orders.user_id")
-			.joins(:order).where(:spree_orders => {:state => "delivery", :user_id => current_api_user.id})
-			
+			@upcoming_orders = Spree::Order.where(:state => "delivery")
 			render "spree/api/orders/mine_upcoming"
 		else
 			render "spree/api/errors/unauthorized", status: :unauthorized
--- app/views/spree/api/orders/mine_past.v1.rabl
+++ app/views/spree/api/orders/mine_past.v1.rabl
@@ -1,24 +1,15 @@
-object @past_line_items
+object @past_orders
 
-attributes :id, :product_id, :box_id, :quantity, :price, :delivery_date
-
-child(:product_item => :product) do
-	attributes :id, :name
-	child(:images => :images) { extends "spree/api/images/show" }
-end
-
-child(:box => :box) do
-	attributes :id, :name
-	node(:images) { "http://www.wbc.co.uk/13662_zoom-large-textured-hamper-box-red.jpg" }
-end
-
-child( :order) do
-	node(:cart_number) {|p| p.number }
-	child(:ship_address) do
+attributes :id, :number
+node(:order_number) { |p| p.number }
+node(:order_date){|p| p.updated_at }
+child(:ship_address) do
 		attributes :id, :user_name, :address1, :district, :city
 	end
-	child(:time_delivery) do
-		node(:time) { |p| p.delivery_time }
-	end
+child(:time_delivery) do
+	node(:time) { |p| p.delivery_time }
 end
 
+child :line_items => :line_items do
+   extends "spree/api/line_items/show"
+end
