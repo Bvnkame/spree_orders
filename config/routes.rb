e1dd5528e9e23e77e5ab8ec4dcc964554fa83715
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-04-14 11:21:32 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
-   get 'api/orders/mine/orders', to: 'api/orders#mine_orders'
+   get 'api/orders/mine/past', to: 'api/orders#mine_past'
    get 'api/orders/mine/upcoming', to: 'api/orders#mine_upcoming'
 end
