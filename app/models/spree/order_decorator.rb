bb2a3497df04a2b397c70075b575fa09a39ab77f
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-05-04 14:57:42 +0700

    update

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,5 +1,31 @@
 Spree::Order.class_eval do 
 
+	ORDER_NUMBER_LENGTH  = 9
+  ORDER_NUMBER_LETTERS = false
+  ORDER_NUMBER_PREFIX  = 'R'
+
+
+	def generate_order_number(options = {})
+    options[:length]  ||= ORDER_NUMBER_LENGTH
+    options[:letters] ||= ORDER_NUMBER_LETTERS
+    options[:prefix]  ||= ORDER_NUMBER_PREFIX
+
+    possible = (0..9).to_a
+    possible += ('A'..'Z').to_a if options[:letters]
+
+    self.number ||= loop do
+      # Make a random number.
+      random = "#{options[:prefix]}#{(0...options[:length]).map { possible.shuffle.first }.join}"
+      # Use the random  number if no other order exists with it.
+      if self.class.exists?(number: random)
+        # If over half of all possible options are taken add another digit.
+        options[:length] += 1 if self.class.count > (10 ** options[:length] / 2)
+      else
+        break random
+      end
+    end
+  end
+
 	def total_price
 		total = 0
 		self.line_items.each do |line_item|
